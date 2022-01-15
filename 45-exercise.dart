f5028b7d9e20e22ad4051800f951286532aaaf40
abubakarshaikh <atifa2019@gmail.com>
2022-01-15 18:59:28 +0500

    some exercise added

---
+++ 45-exercise.dart
@@ -0,0 +1,41 @@
+//1.  Write a Python function to check whether a string is a pangram or not.
+// Note : Pangrams are words or sentences containing every letter of the alphabet at least once.
+// For example : "The quick brown fox jumps over the lazy dog"
+
+// void main() {
+//   isPangram("The quick brown fox jumps over the lazy dog");
+// }
+
+// bool isPangram(String words) {
+//   final List<String> lettes = "abcdefghijklmnopqrstuvwxyz".split("");
+// }
+// void main() {
+// var letters = [for (var i = 65; i <= 90; i++) String.fromCharCode(i)];
+//   print(letters);
+// }
+
+// 2. Write a program in C# Sharp to create a function to input a string and count number of spaces are in the string.
+
+// void main() {
+//   countSpaces("The quick brown fox jumps over the lazy dog");
+// }
+
+// int countSpaces(String sentense) {
+//   return 0;
+// }
+
+
+// 3. Write a program in C# Sharp to create a function to display the n number Fibonacci sequence.
+// Test Data :
+// Input number of Fibonacci Series : 5
+// Expected Output :
+// The Fibonacci series of 5 numbers is :
+// 0 1 1 2 3
+
+// 4. Write a program in C# Sharp to create a function to calculate the sum of the individual digits of a given number.
+// Test Data :
+// Enter a number: 1234
+// Expected Output :
+// The sum of the digits of the number 1234 is : 10
+
+// 5.
